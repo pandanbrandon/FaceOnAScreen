a3447725a8693e36edf62239aeadb522c248574d
pandanbrandon <itspandanbrandon@gmail.com>
2025-05-26 21:23:41 -0500

    first commit

---
+++ dating-app-behav-ds-OG.py
@@ -0,0 +1,32 @@
+'''
+    Project: Dating App Behavior Dataset
+    Description: loads synthetic dataset about user behavior on a dating app
+
+    - find trends, learn something about
+    - maybe visualize some data
+    - BUT, main thing is to create a simple pipeline
+
+    By: Me
+    Date: 5/26/25
+'''
+
+import pandas as pd
+
+# load dataset
+df = pd.read_csv("dating_app_behavior_dataset.csv")
+
+
+# First look
+print(df.info())
+
+
+
+
+
+
+
+
+
+
+
+
